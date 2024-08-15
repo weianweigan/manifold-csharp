00b53a5604cf089bb6751e27e44a3cfb438028f7
weigan <1831197727@qq.com>
2024-08-15 11:54:45 +0800

    add magic command to notebook

--- bindings/csharp/ManifoldNET.Notebooks/ManifoldKernelExtension.cs
+++ bindings/csharp/ManifoldNET.Notebooks/ManifoldKernelExtension.cs
@@ -12,7 +12,16 @@ public static class ManifoldKernelExtension
 {
     public static void Load(Kernel kernel)
     {
-        Formatter.Register<Manifold>((m, writer) => writer.Write(m.ToModelViewer()), "text/html");
+        // Extension for Manifold object.
+        Formatter.Register<Manifold>(
+            (m, writer) => writer.Write(m.ToModelViewer().ToString()),
+            "text/html"
+        );
+
+        // Next, define a magic command that will display glb file.
+        Command glbCommand = CreateGlbMagicCommand();
+
+        kernel.AddDirective(glbCommand);
 
         // Finally, display some information to the user so they can see how to use the extension.
         PocketView view = div(
@@ -20,68 +29,49 @@ public static class ManifoldKernelExtension
             " is loaded. It adds visualizations for ",
             code(typeof(Manifold)),
             ". Try it by running: ",
-            code("Manifold.Cube(1, 1, 1, true)")
+            code("Manifold.Cube(1, 1, 1, true)"),
+            " or using magic code to disply glb file:",
+            code("#!glb -f c:/path/to/name.glb")
         );
 
         KernelInvocationContext.Current?.Display(view);
     }
 
+    private static Command CreateGlbMagicCommand()
+    {
+        var fileOption = new Option<string>(["-f", "--file"], "The glb file local postion.");
+
+        var clockCommand = new Command("#!glb", "Displays a glb file from disk.") { fileOption, };
+
+        clockCommand.SetHandler(
+            (f) =>
+                KernelInvocationContext.Current.DisplayAs(
+                    GenerateGlbHtml(f).ToString(),
+                    "text/html"
+                ),
+            fileOption
+        );
+        return clockCommand;
+    }
+
     public static StringBuilder ToModelViewer(this Manifold manifold)
     {
-        var sb = new StringBuilder();
-
         string tempFile = Path.GetTempFileName();
 
         try
         {
+#if MESH_EXPORT
             manifold.MeshGL.ExportMeshGL(tempFile);
-
-            byte[] fileBytes = System.IO.File.ReadAllBytes(tempFile);
-            string modelData = Convert.ToBase64String(fileBytes);
-
-            sb.Append(
-                """
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/js-base64@3.6.1/base64.min.js"></script>
-                    <style>
-                        html, body {
-                            height: 100%;
-                            margin: 0;
-                            padding: 0;
-                        }
-
-                        model-viewer {
-                            width: 100%;
-                            height: 500px;
-                        }
-                    </style>
-                </head>
-                <body>
-                <h1>test</h1>
-                    <model-viewer id="modelViewer"
-                        autoplay
-                        alt="3D Model"
-                        shadow-intensity="1" camera-controls
-                        touch-action="pan-y" src="data:image/jpeg;base64, 
-                """
+            return GenerateGlbHtml(tempFile);
+#else
+            throw new ManifoldNotebookException(
+                "Cannot export glb file, because Export option did not on!"
             );
-            sb.Append(modelData);
-            sb.Append(
-                """
-                "></model-viewer>
-                </body>
-                </html>
-                """
-            );
+#endif
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            throw;
+            throw new ManifoldNotebookException(tempFile, ex);
         }
         finally
         {
@@ -90,6 +80,52 @@ public static class ManifoldKernelExtension
                 File.Delete(tempFile);
             }
         }
+    }
+
+    private static StringBuilder GenerateGlbHtml(string tempFile)
+    {
+        StringBuilder sb = new();
+        byte[] fileBytes = File.ReadAllBytes(tempFile);
+        string modelData = Convert.ToBase64String(fileBytes);
+
+        sb.Append(
+            """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/js-base64@3.6.1/base64.min.js"></script>
+                <style>
+                    html, body {
+                        height: 100%;
+                        margin: 0;
+                        padding: 0;
+                    }
+
+                    model-viewer {
+                        width: 100%;
+                        height: 500px;
+                    }
+                </style>
+            </head>
+            <body>
+                <model-viewer id="modelViewer"
+                    autoplay
+                    alt="3D Model"
+                    shadow-intensity="1" camera-controls
+                    touch-action="pan-y" src="data:image/jpeg;base64, 
+            """
+        );
+        sb.Append(modelData);
+        sb.Append(
+            """
+            "></model-viewer>
+            </body>
+            </html>
+            """
+        );
 
         return sb;
     }
---
+++ bindings/csharp/ManifoldNET.Notebooks/ManifoldNotebookException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManifoldNET.Notebooks;
+
+[Serializable]
+public class ManifoldNotebookException : Exception
+{
+    public ManifoldNotebookException() { }
+
+    public ManifoldNotebookException(string message)
+        : base(message) { }
+
+    public ManifoldNotebookException(string message, Exception inner)
+        : base(message, inner) { }
+}
